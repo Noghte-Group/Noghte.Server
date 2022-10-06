de0a0aba1ffc0686275695b221395f52742ae063
Erfan Faridi <E.Faridi@Alibaba.ir>
2022-10-06 19:44:29 +0330

    [Add] exception handling

---
+++ Noghte.Server/Noghte.BuildingBlock/Exceptions/BadRequestException.cs
@@ -0,0 +1,36 @@
+﻿namespace Noghte.BuildingBlock.Exceptions;
+
+using System;
+
+public class BadRequestException : AppException
+{
+    public BadRequestException()
+        : base(ConsumerStatusCode.BadRequest, System.Net.HttpStatusCode.BadRequest)
+    {
+    }
+
+    public BadRequestException(string message)
+        : base(ConsumerStatusCode.BadRequest, message, System.Net.HttpStatusCode.BadRequest)
+    {
+    }
+
+    public BadRequestException(object additionalData)
+        : base(ConsumerStatusCode.BadRequest, null, System.Net.HttpStatusCode.BadRequest, additionalData)
+    {
+    }
+
+    public BadRequestException(string message, object additionalData)
+        : base(ConsumerStatusCode.BadRequest, message, System.Net.HttpStatusCode.BadRequest, additionalData)
+    {
+    }
+
+    public BadRequestException(string message, Exception exception)
+        : base(ConsumerStatusCode.BadRequest, message, exception, System.Net.HttpStatusCode.BadRequest)
+    {
+    }
+
+    public BadRequestException(string message, Exception exception, object additionalData)
+        : base(ConsumerStatusCode.BadRequest, message, System.Net.HttpStatusCode.BadRequest, exception, additionalData)
+    {
+    }
+}
